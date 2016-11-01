c3c36251a86ae58831fc4d2e9bbed951880af804
keith brooks <kbrook10@gmail.com>
2016-11-01 09:41:23 -0400

    Update new elements

--- components/Todos.js
+++ components/Todos.js
@@ -9,11 +9,22 @@ class Todos extends Component{ //Todos is the instance and Component is the pare
         this.typing = this.typing.bind(this) //Everything time we make a new method we must use the 'bind'
         this.enter = this.enter.bind(this)
         this.markDone = this.markDone.bind(this)
+        this.updatedTodos = this.updatedTodos.bind(this)
         this.state = { //Whatever does get added to state doesn't get remembered...This sets state of component
             newTodo: '', //Refers to the new Todo string
             todos: [], //Refers to the current array status
         } //These are all the things this component knows about itself...These are little variables that the component knows about itself...Metadata
     }
+    componentDidMount() { //This says do something special code once view on DOM
+        var localTodos = JSON.parse(localStorage.getItem('todos'))
+
+        if (todos) { //This if means we have something...
+            this.setState({
+                todos: localTodos,
+            });
+        }
+    }
+
     typing(e){
         // console.log('typing...')
         this.setState({
@@ -38,10 +49,16 @@ class Todos extends Component{ //Todos is the instance and Component is the pare
     markDone(currentTodoIndex){
         var updatedTodos = this.state.todos //Making copy of todos array
         updatedTodos[currentTodoIndex].done = !updatedTodos[currentTodoIndex].done //Tests 'true' or 'false', first pass the value is 'Not True', which is false
+
+    }
+
+    updateTodos(updatedTodos) {
         this.setState({
             todos: updatedTodos
         })
+            localStorage.setItem('todos', JSON.stringify(updatedTodos))
     }
+
     render(){
         var TaskItems = this.state.todos.map((task, i) =>{ //This 'this.state' references the above instance
             return <TodoItem item={task} key={i} markDone={() => this.markDone(i)} /> //'this.markDone' this calls the function to know which item is being called done...
